ef352458eeae89bcae0150a435ad8fc2310b6c1b
Julio Vinachi <julio899@gmail.com>
2023-04-13 12:09:26 -0400

    control events add checking

--- contracts/TPOA.sol
+++ contracts/TPOA.sol
@@ -8,21 +8,26 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-
 /**
  * @title TPOA (tokens proof of attendance)
  * @version: v1.0
- * TopacioTrade Contracts (last updated v1.0.0)
+ * TopacioTrade Contracts (last updated v1.0.1)
  * Network: Polygon
  */
- 
- /**
+
+/**
  * @dev If you want to participate in or support decentralized projects like this
  * you can visit https://www.topacio.trade
  */
 
 /// @custom:security-contact jv@topacio.trade
-contract TPoa is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Burnable, Ownable {
+contract TPoa is
+    ERC721,
+    ERC721Enumerable,
+    ERC721URIStorage,
+    ERC721Burnable,
+    Ownable
+{
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIdCounter;
     string public _baseURI_ipfs = "https://ipfs.io/ipfs/";
@@ -30,35 +35,37 @@ contract TPoa is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Burnable, Own
     mapping(address => uint256) private _administradores;
     mapping(uint256 => uint256) public _metadata;
     // A mapping of token owners Holders
-    mapping(uint => address[]) public ownersHolders;
-    mapping(uint => string) public hash_ipfs_img;
+    mapping(uint256 => address[]) public ownersHolders;
+    mapping(uint256 => string) public hash_ipfs_img;
 
     struct Burned {
-        uint a;  
+        uint256 a;
         address w;
     }
 
-    mapping(uint => Burned[]) public burned;
-    mapping(address => uint) private holderList;
-    uint private holderCtr;
-    uint public burnedCtr;
+    mapping(uint256 => Burned[]) public burned;
+    mapping(address => uint256) private holderList;
+    uint256 private holderCtr;
+    uint256 public burnedCtr;
 
     // address, tokenId
-    event NftBurnned(address, uint);
+    event NftBurnned(address, uint256);
 
-    event NewMint(address,uint256);
+    event NewMint(address, uint256);
 
     modifier onlyAdministrator() {
-         require(_administradores[msg.sender]!=0,"you are not administrator");
+        require(_administradores[msg.sender] != 0, "you are not administrator");
         _;
     }
 
-    constructor(string memory __name,string memory __symbol) ERC721(__name, __symbol) {
+    constructor(string memory __name, string memory __symbol)
+        ERC721(__name, __symbol)
+    {
         _administradores[msg.sender] = 1;
         isActive = true;
     }
 
-    function getHashImg(uint _idTkn) public view returns (string memory){
+    function getHashImg(uint256 _idTkn) public view returns (string memory) {
         return hash_ipfs_img[_idTkn];
     }
 
@@ -66,47 +73,54 @@ contract TPoa is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Burnable, Own
         return _baseURI_ipfs;
     }
 
-    function updateBaseURI(string memory _uri_base_ipfs ) public onlyOwner {
+    function updateBaseURI(string memory _uri_base_ipfs) public onlyOwner {
         _baseURI_ipfs = _uri_base_ipfs;
     }
 
-    function safeMint(address to, string memory uri, string memory hash_img) public onlyAdministrator {
-        require(isActive==true,"It's not active");
+    function safeMint(
+        address to,
+        string memory uri,
+        string memory hash_img
+    ) public onlyAdministrator {
+        require(isActive == true, "It's not active");
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, uri);
         hash_ipfs_img[tokenId] = hash_img;
-        emit NewMint(to,tokenId);
+        emit NewMint(to, tokenId);
     }
 
-    function getHoldersCount() public view returns(uint){
-       return  holderCtr;
+    function getHoldersCount() public view returns (uint256) {
+        return holderCtr;
     }
 
     function getHolders() public view returns (address[] memory) {
         address[] memory holders_w = new address[](holderCtr);
-        uint pp=0;
-        for(uint zz=0; zz < ownersHolders[0].length ;zz++){
-            if(ownersHolders[0][zz]!=address(0)){
-                holders_w[pp]=(ownersHolders[0][zz]);
+        uint256 pp = 0;
+        for (uint256 zz = 0; zz < ownersHolders[0].length; zz++) {
+            if (ownersHolders[0][zz] != address(0)) {
+                holders_w[pp] = (ownersHolders[0][zz]);
                 pp++;
             }
         }
 
         return holders_w;
-
     }
 
-    function getTokensHolder( address _wallet) public view returns (uint[] memory) {
-        require(_wallet != address(0),"wallet can't be zero");
-        uint balanceW = balanceOf(_wallet);
- 
-        uint[] memory tkns_w = new uint[](balanceW);
-        if(balanceW>0){
-            for(uint x = 0; x < balanceW; x++){
-                uint tId = tokenOfOwnerByIndex(_wallet, x);
-                tkns_w[x]=(tId);
+    function getTokensHolder(address _wallet)
+        public
+        view
+        returns (uint256[] memory)
+    {
+        require(_wallet != address(0), "wallet can't be zero");
+        uint256 balanceW = balanceOf(_wallet);
+
+        uint256[] memory tkns_w = new uint256[](balanceW);
+        if (balanceW > 0) {
+            for (uint256 x = 0; x < balanceW; x++) {
+                uint256 tId = tokenOfOwnerByIndex(_wallet, x);
+                tkns_w[x] = (tId);
             }
         }
 
@@ -114,43 +128,49 @@ contract TPoa is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Burnable, Own
     }
 
     function removeOfHolders(address _w) internal {
-        for(uint z=0; z < ownersHolders[0].length ;z++){
-            if(ownersHolders[0][z]==_w){
-                delete(ownersHolders[0][z]);
-                if(holderCtr>0){
+        for (uint256 z = 0; z < ownersHolders[0].length; z++) {
+            if (ownersHolders[0][z] == _w) {
+                delete (ownersHolders[0][z]);
+                if (holderCtr > 0) {
                     holderCtr--;
                 }
             }
         }
     }
 
-
     // The following functions are overrides required by Solidity.
 
-    function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
-        internal
-        override(ERC721, ERC721Enumerable)
-    { 
-        require(from == address(0),"can't be transfer");
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId,
+        uint256 batchSize
+    ) internal override(ERC721, ERC721Enumerable) {
+        require(from == address(0), "can't be transfer");
         bool isZero = (to == address(0));
 
-        if(from!=address(0) && from!=address(this) && balanceOf(from)==1 ){
+        if (
+            from != address(0) && from != address(this) && balanceOf(from) == 1
+        ) {
             removeOfHolders(from);
         }
 
         // nuevo Holder
-        if(!isZero && balanceOf(to)==0){
+        if (!isZero && balanceOf(to) == 0) {
             ownersHolders[0].push(to);
             holderCtr++;
         }
-        
+
         super._beforeTokenTransfer(from, to, tokenId, batchSize);
     }
 
-    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+    function _burn(uint256 tokenId)
+        internal
+        override(ERC721, ERC721URIStorage)
+    {
         super._burn(tokenId);
         burnedCtr++;
-        burned[burnedCtr].push(Burned(burnedCtr,msg.sender));
+        burned[burnedCtr].push(Burned(burnedCtr, msg.sender));
         emit NftBurnned(msg.sender, tokenId);
     }
 
@@ -163,30 +183,34 @@ contract TPoa is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Burnable, Own
         return super.tokenURI(tokenId);
     }
 
-    function updateTokenURI(
-        uint256 tokenId, 
-        string memory _tokenURI
-    ) external onlyAdministrator {
+    function updateTokenURI(uint256 tokenId, string memory _tokenURI)
+        external
+        onlyAdministrator
+    {
         super._setTokenURI(tokenId, _tokenURI);
     }
 
-    function checkAdmin(address admin) public view onlyAdministrator returns (uint256){
+    function checkAdmin(address admin)
+        public
+        view
+        onlyAdministrator
+        returns (uint256)
+    {
         return _administradores[admin];
     }
 
-    function addAdmin(address admin) public onlyOwner{
-        _administradores[admin]=1;
+    function addAdmin(address admin) public onlyOwner {
+        _administradores[admin] = 1;
     }
 
     function removeAdmin(address admin) public onlyOwner {
-        delete(_administradores[admin]);
+        delete (_administradores[admin]);
     }
 
     function lastToken() public view returns (uint256) {
-        require(_tokenIdCounter.current() > 0,"no exist nfts until now");
+        require(_tokenIdCounter.current() > 0, "no exist nfts until now");
         return _tokenIdCounter.current() - 1;
     }
-
 
     function supportsInterface(bytes4 interfaceId)
         public
@@ -198,74 +222,150 @@ contract TPoa is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Burnable, Own
     }
 }
 
-contract AdministratorTPoa{
+contract AdministratorTPoa {
     TPoa[] tpoas;
     string __name;
     string __symbol_prefix;
-    uint control_tpoas;
-    uint block_minutes_active;
-    uint public totalMint;
+    uint256 control_tpoas;
+    uint256 block_minutes_active;
+    uint256 public totalMint;
     address owner;
-    
-    mapping(address => mapping(uint => uint256[])) public TPoaInAddress;
-    mapping(address => mapping(uint => string)) public TPoaMetaData;
-    mapping(address => mapping(uint => string)) public TPoaImgs;
+
+    mapping(address => mapping(uint256 => uint256[])) public TPoaInAddress;
+    mapping(address => mapping(uint256 => string)) public TPoaMetaData;
+    mapping(address => string) public TPoaImgs;
+    // Indexed parameters helps you filter the logs by the indexed parameter
+    event EvtMint(address indexed _to);
+    event NewMint(address indexed _to, uint256 tkn, address contractTpoa);
 
     modifier onlyOwner() {
-         require(msg.sender==owner,"you are not owner");
+        require(msg.sender == owner, "you are not owner");
         _;
     }
 
-    constructor(string memory name_collectrion,string memory symbol_collection) payable {
+    constructor(string memory name_collectrion, string memory symbol_collection)
+        payable
+    {
         __name = name_collectrion;
         __symbol_prefix = symbol_collection;
         owner = msg.sender;
     }
-    
-    function addNewTPoa(string memory __uri_hash_meta,string memory __hash_img,uint __minutes_active) public payable onlyOwner {
-        TPoa newTPoa = new TPoa(__name,__symbol_prefix);
+
+    function isOwner() public view returns (bool) {
+        return owner == msg.sender;
+    }
+
+    function addNewTPoa(
+        string memory __uri_hash_meta,
+        string memory __hash_img,
+        uint256 __minutes_active
+    ) public payable onlyOwner {
+        TPoa newTPoa = new TPoa(__name, __symbol_prefix);
         tpoas.push(newTPoa);
-        TPoaImgs[address(newTPoa)][control_tpoas] = __hash_img;
+        TPoaImgs[address(newTPoa)] = __hash_img;
         TPoaMetaData[address(newTPoa)][control_tpoas] = __uri_hash_meta;
-        block_minutes_active =  block.timestamp + (__minutes_active * 1 minutes);
+        block_minutes_active = block.timestamp + (__minutes_active * 1 minutes);
         control_tpoas++;
     }
 
-    function mintPublic() public payable {
-        require(block_minutes_active > block.timestamp,"Tpoa the time has finished");
-        require(tpoas.length>0,"need to have some collection created");
-        require( tpoas[control_tpoas-1].balanceOf(msg.sender)==0,"this address has this tpoa");
-        require( address(this).balance > 0,"Contract Administrator needed some Gas fees.");
-        
-        tpoas[control_tpoas-1].safeMint(
-            msg.sender,
-            TPoaMetaData[address(tpoas[control_tpoas-1])][control_tpoas-1],
-            TPoaImgs[address(tpoas[control_tpoas-1])][control_tpoas-1]
+    function getImageHashTpoa(address _tpoa_address)
+        public
+        view
+        returns (string memory)
+    {
+        return TPoaImgs[address(_tpoa_address)];
+    }
+
+    function mintExternal(address destine) external payable onlyOwner {
+        require(
+            block_minutes_active > block.timestamp,
+            "Tpoa the time has finished"
         );
-        
-        uint idTk = tpoas[control_tpoas-1].lastToken();
-        TPoaInAddress[msg.sender][control_tpoas-1].push(idTk);
+        require(tpoas.length > 0, "need to have some collection created");
+        require(
+            tpoas[control_tpoas - 1].balanceOf(destine) == 0,
+            "this address has this tpoa"
+        );
+        require(
+            address(destine).balance > 0,
+            "Needed some Gas fees."
+        );
+
+        tpoas[control_tpoas - 1].safeMint(
+            destine,
+            TPoaMetaData[address(tpoas[control_tpoas - 1])][control_tpoas - 1],
+            TPoaImgs[address(tpoas[control_tpoas - 1])]
+        );
+
+        uint256 idTk = tpoas[control_tpoas - 1].lastToken();
+        TPoaInAddress[destine][control_tpoas - 1].push(idTk);
+        emit NewMint(destine, idTk, address(tpoas[control_tpoas - 1]));
         totalMint++;
     }
 
-    function mintTPoa(address _to,string memory _uri, string memory hash_img) public payable onlyOwner{
-        require(tpoas[control_tpoas-1].balanceOf(_to)==0,"this address has this tpoa");        
-        tpoas[control_tpoas-1].safeMint(_to,_uri,hash_img);
-        uint idTk = tpoas[control_tpoas-1].lastToken();
-        TPoaInAddress[_to][control_tpoas-1].push(idTk);
+    function checking() public {
+        require(
+            block_minutes_active > block.timestamp,
+            "Tpoa the time has finished"
+        );
+        require(tpoas.length > 0, "need to have some collection created");
+        require(
+            tpoas[control_tpoas - 1].balanceOf(msg.sender) == 0,
+            "this address has this tpoa"
+        );
+        emit EvtMint(msg.sender);
+    }
+
+    function mintPublic() public payable {
+        require(
+            block_minutes_active > block.timestamp,
+            "Tpoa the time has finished"
+        );
+        require(tpoas.length > 0, "need to have some collection created");
+        require(
+            tpoas[control_tpoas - 1].balanceOf(msg.sender) == 0,
+            "this address has this tpoa"
+        );
+
+        tpoas[control_tpoas - 1].safeMint(
+            msg.sender,
+            TPoaMetaData[address(tpoas[control_tpoas - 1])][control_tpoas - 1],
+            TPoaImgs[address(tpoas[control_tpoas - 1])]
+        );
+
+        uint256 idTk = tpoas[control_tpoas - 1].lastToken();
+        TPoaInAddress[msg.sender][control_tpoas - 1].push(idTk);
         totalMint++;
     }
 
-    function getCountsTPoaOfCollections(uint _n) public view returns(uint){
+    function mintTPoa(
+        address _to,
+        string memory _uri,
+        string memory hash_img
+    ) public payable onlyOwner {
+        require(
+            tpoas[control_tpoas - 1].balanceOf(_to) == 0,
+            "this address has this tpoa"
+        );
+        tpoas[control_tpoas - 1].safeMint(_to, _uri, hash_img);
+        uint256 idTk = tpoas[control_tpoas - 1].lastToken();
+        TPoaInAddress[_to][control_tpoas - 1].push(idTk);
+        totalMint++;
+    }
+
+    function getCountsTPoaOfCollections(uint256 _n)
+        public
+        view
+        returns (uint256)
+    {
         return tpoas[_n].totalSupply();
     }
 
-    function getTPoaCollections() public view returns(TPoa[] memory){
+    function getTPoaCollections() public view returns (TPoa[] memory) {
         return tpoas;
     }
 
-    function getCountTPoaCollections() public view returns(uint){
+    function getCountTPoaCollections() public view returns (uint256) {
         return tpoas.length;
     }
-
 }
